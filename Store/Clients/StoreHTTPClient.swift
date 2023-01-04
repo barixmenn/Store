1066e09d0db903220e0b35e06f2dc6c2008498fd
baris dilekci <bdilekci200@gmail.com>
2023-01-04 14:45:37 +0300

    The data from the json is listed in the tableview.

--- Store/Clients/StoreHTTPClient.swift
+++ Store/Clients/StoreHTTPClient.swift
@@ -20,7 +20,7 @@ class StoreHTTPClient {
         let (data, response) = try await URLSession.shared.data(from: URL.allCategories)
         
         guard let httpResponse = response as? HTTPURLResponse,
-              httpResponse.statusCode == 200
+              httpResponse.statusCode == 200 /// - ok: Standard response for successful HTTP requests.
         else {
             throw NetworkError.invalidServerResponse
         }
--- Store/Controllers/CategoriesTableViewController.swift
+++ Store/Controllers/CategoriesTableViewController.swift
@@ -7,14 +7,74 @@
 
 import UIKit
 
+
+
 class CategoriesTableViewController: UITableViewController {
 
+    //MARK: - Properties
+    private var client = StoreHTTPClient()
+    private var categories: [Category] = []
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CategoryCell")
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Categories"
 
-        view.backgroundColor = .brown
+
+        Task {
+            await populateCategories()
+            tableView.reloadData()
+        }
     }
 
+    //MARK: - Functions
     
+    private func populateCategories() async{
+        do {
+            categories =  try await client.getAllCategories()
+            print(categories)
+        }catch {
+            // Error message
+        }
+    }
+    
+    
+    
+    //MARK: - Tableview properties
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        
+        let category = categories[indexPath.row]
+        
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = category.name
+        
+        if let url = URL(string: category.image) {
+            DispatchQueue.global().async {
+                if let data = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        let image = UIImage(data: data)
+                        configuration.image = image
+                        configuration.imageProperties.maximumSize = CGSize(width: 75, height: 75)
+                        cell.contentConfiguration = configuration
+                    }
+                }
+            }
+        }
+        
+        
+       
+        
+        return cell
+    }
     
 }
+
